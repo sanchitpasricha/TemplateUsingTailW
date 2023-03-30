f03f810b44a8cac05f20ab3b6ac16275337559b8
sanchitpasricha <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>
2023-03-31 04:15:00 +0530

    Initial commit with header componet

---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineCloseCircle } from "react-icons/ai";
+
+function Header() {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <div className="bg-[#2699fb] p-4">
+      <div className="max-w-[1240px] py-[8px] items-center flex justify-between mx-auto">
+        <div className="text-3xl font-bold">Tech.io</div>
+        {toggle ? (
+          <AiOutlineMenu
+            onClick={() => setToggle(!toggle)}
+            className="text-white text-2xl md:hidden block"
+          />
+        ) : (
+          <AiOutlineCloseCircle
+            onClick={() => setToggle(!toggle)}
+            className="text-white text-2xl md:hidden block"
+          />
+        )}
+        <ul className="hidden md:flex text-white gap-10">
+          <li>Home</li>
+          <li>Company</li>
+          <li>Resources</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+        <ul className="md:hidden w-full h-screen text-white fixed bg-black top-[84px] left-[0]">
+          <li className="p-5">Home</li>
+          <li className="p-5">Company</li>
+          <li className="p-5">Resources</li>
+          <li className="p-5">About</li>
+          <li className="p-5">Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
